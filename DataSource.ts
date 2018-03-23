9850759db48959f1b45b628ab814914ff359e699
Vitaly Arkhipov <arvitaly@list.ru>
2018-03-23 09:41:50 +0300

    added onInitialData

--- DataSource.ts
+++ DataSource.ts
@@ -7,9 +7,17 @@ export interface IDataSourceConfig<D, P, R> {
 export default class DataSource<D, P, R> extends Onemitter<D> {
     constructor(protected config: IDataSourceConfig<D, P, R>) {
         super();
+        if (typeof (config.data) !== "undefined") {
+            this.onInitialData(config.data);
+        }
         this.onInit();
     }
+    public onInitialData(value: D) {
+        this.emit(value);
+    }
     public onInit() {
-        this.emit(null as any);
+        if (typeof (this.config.data) === "undefined") {
+            this.emit(null as any);
+        }
     }
 }
